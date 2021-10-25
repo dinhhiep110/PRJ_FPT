53f0da78113f26ce1cf1d65af9213f8bf45f68d6
Duy Hiep <dinhhiep110@gmail.com>
2021-10-25 11:27:49 +0700

    Commit

---
+++ JDBC_Demo/src/java/controller/filter/AuthenticationFilter.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/AuthenticationFilter")
+public class AuthenticationFilter implements Filter {
+
+	private ServletContext context;
+	
+        @Override
+	public void init(FilterConfig fConfig) throws ServletException {
+		this.context = fConfig.getServletContext();
+		this.context.log("AuthenticationFilter initialized");
+	}
+	
+        @Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
+		String uri = req.getRequestURI();
+		this.context.log("Requested Resource::"+uri);
+		
+		HttpSession session = req.getSession(false);
+		
+		if(session == null ){
+			this.context.log("Unauthorizedaccess request"); 
+                        res.getWriter().println("Unauthorizedaccess request");
+			res.sendRedirect("../login");
+		}else{
+			// pass the request along the filter chain
+			chain.doFilter(request, response);
+		}
+		
+		
+	}
+
+        @Override
+	public void destroy() {
+		//close any resources here
+	}
+
+}
